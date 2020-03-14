83568b9017aecdf50ac993a7feef3c027b9b16f8
KavyaPilli <pillikavya97@gmail.com>
2020-03-14 14:43:16 +0530

    Annotations

---
+++ annotation/src/main/java/com/hibernate/annotation/annotation/TestMain.java
@@ -0,0 +1,34 @@
+package com.hibernate.annotation.annotation;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();  
+	    Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();  
+	  
+	SessionFactory factory = meta.getSessionFactoryBuilder().build();  
+	Session session = factory.openSession();  
+	Transaction t = session.beginTransaction();   
+	            
+	    Employee e1=new Employee();    
+	    e1.setId(101);    
+	    e1.setFirstName("Kavya");    
+	    e1.setLastName("Pilli");    
+	        
+	    session.save(e1);  
+	    t.commit();  
+	    System.out.println("successfully saved");    
+	    factory.close();  
+	    session.close();    
+	}
+
+}
