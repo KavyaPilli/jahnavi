27928ec0a3862a6b38c669e86d71866422207013
KavyaPilli <pillikavya97@gmail.com>
2020-03-17 15:39:22 +0530

    Webproject

---
+++ webproject/src/LoginServlet.java
@@ -0,0 +1,53 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		String ui=request.getParameter("uid");
+		String pw=request.getParameter("pwd");
+		PrintWriter out=response.getWriter();
+		out.println("<html><body>");
+		if(ui.equals("admin")&& pw.equals("1234"))
+			out.println("<b> Valid Users </b>");
+		else
+			out.println("<b> Invalid Users </b>");
+		out.println("</body></html>");
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
