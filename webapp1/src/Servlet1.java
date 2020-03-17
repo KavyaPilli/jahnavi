ff509e046dd180bbf3ffce2baee1066e50f3e6c7
KavyaPilli <pillikavya97@gmail.com>
2020-03-17 15:38:49 +0530

    webapp1

---
+++ webapp1/src/Servlet1.java
@@ -0,0 +1,55 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet1
+ */
+@WebServlet("/Servlet1")
+public class Servlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter pw=response.getWriter();
+		pw.println("<h2>Im from Servlet1 beginning </h2>");
+		ServletContext ctx=this.getServletContext();
+		ServletContext sctx=ctx.getContext("/webapp2");
+		RequestDispatcher rd=sctx.getRequestDispatcher("/Servlet2");
+	
+		rd.forward(request,response);
+		pw.println("<h2> Im from Serv1 Ending</h2>");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
