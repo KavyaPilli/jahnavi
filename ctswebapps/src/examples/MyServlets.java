58a46d646688f46e06c15f1efe3a6ee32122fae9
KavyaPilli <pillikavya97@gmail.com>
2020-03-17 15:26:46 +0530

    CtsWebApps

---
+++ ctswebapps/src/examples/MyServlets.java
@@ -0,0 +1,42 @@
+package examples;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+/**
+ * Servlet implementation class MyServlets
+ */
+@WebServlet("/MyServlets")
+public class MyServlets extends GenericServlet implements Servlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see GenericServlet#GenericServlet()
+     */
+    public MyServlets() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#service(ServletRequest request, ServletResponse response)
+	 */
+
+public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+response.setContentType("text/html");
+PrintWriter out=response.getWriter();
+out.println("<html><body>");
+out.println("<b> Welcome to servlet programming </b>");
+out.println("</body></html>");
+out.close();
+
+	}
+
+}
